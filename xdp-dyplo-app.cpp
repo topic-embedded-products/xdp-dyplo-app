7cf4c7c365e3d13ebb73b9e5e98a052c32aaf1e3
Mike Looijmans <mike.looijmans@topic.nl>
2019-10-14 15:47:15 +0200

    Generic application that can write to file, stdout or framebuffer
    Options to set image dimension and DMA method

--- xdp-dyplo-app.cpp
+++ xdp-dyplo-app.cpp
@@ -21,9 +21,52 @@
 #define DYPLO_NODE_CAMERA_0	0
 //#define DYPLO_NODE_CAMERA_1	3
 
-static const unsigned int video_size_pixels = 1920 * 1080;
-static const unsigned int video_bytes_per_pixel = 4; /* RGBX */
-static const unsigned int video_size_bytes = video_size_pixels * video_bytes_per_pixel;
+static void usage(const char* name)
+{
+	std::cerr << "usage: " << name << " [-c #] [-d destination|-f file] [-v] [-s] [-w width] [-h height] [-b bpp] [-k skip]\n"
+		" -c    Camera node index, default: 0\n"
+		" -d    Destination framebuffer (mmapped), default: /dev/fb0\n"
+		" -f    Output to file instead of memory mapped, - for stdout\n"
+		" -s    Streaming DMA mode (only if frame size less than 4MB)\n"
+		" -w    Frame width in pixels, default: 1920\n"
+		" -h    Frame height in lines, default: 1080\n"
+		" -b    Bits per pixel, default: 32\n"
+		" -v    Verbose mode, output stats\n"
+		" -k    Skip frames\n"
+		"\n";
+}
+
+
+#include <time.h>
+
+class Stopwatch
+{
+public:
+        struct timespec m_start;
+        struct timespec m_stop;
+
+        Stopwatch()
+        {
+                clock_gettime(CLOCK_MONOTONIC, &m_start);
+        }
+
+        void start()
+        {
+                clock_gettime(CLOCK_MONOTONIC, &m_start);
+        }
+
+        void stop()
+        {
+                clock_gettime(CLOCK_MONOTONIC, &m_stop);
+        }
+
+        unsigned int elapsed_us()
+        {
+                return
+                        ((m_stop.tv_sec - m_start.tv_sec) * 1000000) +
+                                (m_stop.tv_nsec - m_start.tv_nsec) / 1000;
+        }
+};
 
 static inline void fcntl_set_flag(int handle, long flag)
 {
@@ -42,7 +85,6 @@ static void* mmap_file(int handle, int prot, off_t offset, size_t size)
 	return map;
 }
 
-
 int main(int argc, char** argv)
 {
 	unsigned int frames_captured = 0;
@@ -53,8 +95,76 @@ int main(int argc, char** argv)
 
 	try
 	{
-		dyplo::File framebuffer(::open("/dev/fb0", O_RDWR));
-		void *fb = mmap_file(framebuffer.handle, PROT_READ | PROT_WRITE, 0, video_size_bytes);
+		const char *fb_name = "/dev/fb0";
+		int camera_node = DYPLO_NODE_CAMERA_0;
+		bool verbose = false;
+		bool streaming = false;
+		bool mmap_framebuffer = true;
+		unsigned int video_width = 1920;
+		unsigned int video_height = 1080;
+		unsigned int video_bytes_per_pixel = 4; /* RGBX */
+		int skip_frames = 7;
+
+
+		for (;;)
+		{
+			int c = getopt(argc, argv, "b:c:d:f:h:k:svw:");
+			if (c < 0)
+				break;
+			switch (c)
+			{
+			case 'b':
+				video_bytes_per_pixel = strtoll(optarg, NULL, 0) / 8;
+				break;
+			case 'c':
+				camera_node = strtoll(optarg, NULL, 0);
+				break;
+			case 'd':
+				mmap_framebuffer = true;
+				fb_name = optarg;
+				break;
+			case 'f':
+				mmap_framebuffer = false;
+				fb_name = optarg;
+				break;
+			case 'h':
+				video_height = strtoll(optarg, NULL, 0);
+				break;
+			case 'k':
+				skip_frames = strtoll(optarg, NULL, 0);
+				break;
+			case 's':
+				streaming = true;
+				break;
+			case 'v':
+				verbose = true;
+				break;
+			case 'w':
+				video_width = strtoll(optarg, NULL, 0);
+				break;
+			case '?':
+				usage(argv[0]);
+				return 1;
+			}
+		}
+
+		const unsigned int video_size_bytes = video_width * video_height * video_bytes_per_pixel;
+
+		int framebuffer_fd;
+		if (mmap_framebuffer)
+			framebuffer_fd = ::open(fb_name, O_RDWR);
+		else
+			if (!strcmp(fb_name, "-"))
+				framebuffer_fd = 1; /* Causes stdout to be closed later, but we don't care */
+			else
+				framebuffer_fd = ::open(fb_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		dyplo::File framebuffer(framebuffer_fd);
+		
+		void *fb;
+		if (mmap_framebuffer)
+			fb = mmap_file(framebuffer.handle, PROT_READ | PROT_WRITE, 0, video_size_bytes);
+		else
+			fb = NULL;
 
 		// Create objects for hardware control
 		dyplo::HardwareContext hardware;
@@ -62,14 +172,16 @@ int main(int argc, char** argv)
 
 		/* Open the DMA channel */
 		dyplo::HardwareDMAFifo from_camera(hardware.openDMA(0, O_RDONLY));
-		from_camera.addRouteFrom(DYPLO_NODE_CAMERA_0);
+		from_camera.addRouteFrom(camera_node);
 
 		/* Allocate buffers, because of the zero-copy system, the driver
 		 * will allocate them for us in DMA capable memory, and give us
 		 * direct access through a memory map. The library does all the
 		 * work for us. */
 		static const unsigned int num_blocks = 8;
-		from_camera.reconfigure(dyplo::HardwareDMAFifo::MODE_COHERENT, video_size_bytes, num_blocks, true);
+		from_camera.reconfigure(
+			streaming ? dyplo::HardwareDMAFifo::MODE_STREAMING : dyplo::HardwareDMAFifo::MODE_COHERENT,
+			video_size_bytes, num_blocks, true);
 
 		/* Prime the reader with empty blocks. Just dequeue all blocks
 		 * and enqueue them. */
@@ -81,59 +193,43 @@ int main(int argc, char** argv)
 		}
 
 		/* Non-blocking IO */
-		from_camera.fcntl_set_flag(O_NONBLOCK);
+		// from_camera.fcntl_set_flag(O_NONBLOCK);
+		
+		Stopwatch s;
 
 		for (;;)
 		{
-			dyplo::HardwareDMAFifo::Block *block = NULL;
-			/* Fetch the frame */
-			for(;;)
+			for (int i = 0; i < skip_frames; ++i)
 			{
-				dyplo::HardwareDMAFifo::Block *nextblock = from_camera.dequeue();
-				if (nextblock == NULL)
-				{
-					//std::cerr << "dequeue NULL\n";
-					/* Would block */
-					if (block)
-						break; /* Got a frame */
-					/* No frame, must wait */
-					std::cerr << "Frames: " << frames_captured <<
-						" Invalid: " << frames_incomplete <<
-						" Sent: " << frames_sent <<
-						std::endl;
-					from_camera.poll_for_incoming_data(1);
-					continue;
-				}
-				//std::cerr << "dequeue " << nextblock->id << '\n';
-
-				if (block)
-				{
-					//std::cerr << "enqueue o " << block->id << '\n';
-					block->bytes_used = video_size_bytes;
-					from_camera.enqueue(block);
-					block = NULL;
-				}
-
+				dyplo::HardwareDMAFifo::Block *block = from_camera.dequeue();
 				++frames_captured;
-				if (nextblock->bytes_used != video_size_bytes)
-				{
-					//std::cerr << "enqueue i " << nextblock->id << '\n';
-					nextblock->bytes_used = video_size_bytes;
-					from_camera.enqueue(nextblock);
+				if (block->bytes_used != video_size_bytes)
 					++frames_incomplete;
-					continue;
-				}
-
-				/* Candidate arrived: A full frame, see if there are more... */
-				block = nextblock;
+				block->bytes_used = video_size_bytes;
+				from_camera.enqueue(block);
 			}
 
-			++frames_sent;
-			memcpy(fb, block->data, video_size_bytes);
-
-			//std::cerr << "enqueue s " << block->id << '\n';
-			block->bytes_used = video_size_bytes;
-			from_camera.enqueue(block);
+			{
+				dyplo::HardwareDMAFifo::Block *block;
+				block = from_camera.dequeue();
+				++frames_captured;
+				if (block->bytes_used == video_size_bytes)
+				{
+					++frames_sent;
+					s.start();
+					if (fb)
+						memcpy(fb, block->data, video_size_bytes);
+					else
+						framebuffer.write(block->data, video_size_bytes);
+					s.stop();
+					if (verbose)
+						std::cerr << "memcpy: " << s.elapsed_us() << '\n';
+				}
+				else
+					++frames_incomplete;
+				block->bytes_used = video_size_bytes;
+				from_camera.enqueue(block);
+			}
 		}
 	}
 	catch (const std::exception& ex)
